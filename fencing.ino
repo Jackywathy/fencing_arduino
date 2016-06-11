80920c54e3587b8dee3f750cefd7745134bc20b2
Jack Jiang <jackywathy24@gmail.com>
2016-06-11 21:04:05 +1000

    DONE!

--- fencing.ino
+++ fencing.ino
@@ -4,28 +4,29 @@
 
 
 /* pinout: TODO FINISH THIS CUZ ITS REALLY BORING!
- *  
- *  
- *  
- *  
- *  
- *  
- *  
- *  
- *  
- *  
- */
+NOT ALL PINS ARE THE SAME! try find one with less resistance so i chose 2,4,A5 amd A3
+
+
+
+
+
+
+
+
+
+*/
 // pins
-#define SWORDINTERRUPT 3
+#define SWORDINTERRUPT 7
 
-#define LEFTPLAYER_IN 5
-#define RIGHTPLAYER_IN 6
-
-#define LEFTPLAYER_OUT 8
-#define RIGHTPLAYER_OUT 9
-
-#define LEFTPLAYER_LAME 11
-#define RIGHTPLAYER_LAME 12
+#define LEFTPLAYER_WEAPON 2
+#define RIGHTPLAYER_WEAPON 4
+#define LEFTPLAYER_LAME A2
+#define RIGHTPLAYER_LAME A5
+//
+#define LEFTPLAYER_OUT 9
+#define RIGHTPLAYER_OUT 8
+#define LEFTPLAYER_OFF 10
+#define RIGHTPLAYER_OFF 11
 
 
 
@@ -34,9 +35,9 @@
 // ms of a double hit (usually 40-50)
 #define LIGHTON 2000
 // ms of how long the light will stay on (may be ~60ish ms off but who cares
-#define PAUSETIME 400
+#define PAUSETIME 250
 // ms of how long the pause is
-#define BEEPLENGTH 3000
+#define BEEPLENGTH 1500
 // lenght of beep in ms
 
 
@@ -55,103 +56,195 @@ void foil(void);
 void epee(void);
 
 
-int detect_player(int pin){
-  // detects if the player given was hit
-  if (digitalRead(pin) == LOW){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-int wait_hit(int player){
-  // returns true if both sides got hit in DOUBLE_TIME time
-  int test;
-  if (player == LEFT){
-    test = RIGHTPLAYER_IN;
-  }
-  else{
-    test = LEFTPLAYER_IN;
-  }
-
- 
-  timer0 = 0;
-  while (timer0 <= DOUBLE_TIME){
-    if (detect_player(test) == true){
-      Serial.print(test);
-      return true;
-    }
-  }
-  return false;
-  
-}
-
-void turn_off(void){
+void turn_off(void) {
   delay(LIGHTON);
   digitalWrite(LEFTPLAYER_OUT, LOW);
   digitalWrite(RIGHTPLAYER_OUT, LOW);
+  digitalWrite(LEFTPLAYER_OFF, LOW);
+  digitalWrite(RIGHTPLAYER_OFF, LOW);
+
   Serial.println("offlights");
   delay(PAUSETIME);
 }
 
-void sword_choose(void){
-  if (weapon_type < 3){
-    weapon_type++;}
-  else if (weapon_type == 3){
+void sword_choose(void) {
+  if (weapon_type < 3) {
+    weapon_type++;
+  }
+  else if (weapon_type == 3) {
     weapon_type = 0;
   }
+
+  if (weapon_type == 0) {
+    foil();
+  }
+  else if (weapon_type == 1) {
+    sabre();
+  }
+  else {
+    epee();
+  }
+
+}
+volatile bool weapon_change = false;
+unsigned long mic = micros();
+
+void setup() {
+  pinMode(LEFTPLAYER_WEAPON, INPUT_PULLUP);
+  pinMode(RIGHTPLAYER_WEAPON, INPUT_PULLUP);
+
+  pinMode(LEFTPLAYER_LAME, INPUT_PULLUP);
+  pinMode(RIGHTPLAYER_LAME, INPUT_PULLUP);
+
+  pinMode(RIGHTPLAYER_OUT, OUTPUT);
+  pinMode(LEFTPLAYER_OUT, OUTPUT);
+
+  pinMode(RIGHTPLAYER_OFF, OUTPUT);
+  pinMode(LEFTPLAYER_OFF, OUTPUT);
+
+
+
+  Serial.begin(9600);
+  Serial.print("Hello world.");
+
+  //attachInterrupt(digitalPinToInterrupt(SWORDINTERRUPT), sword_choose, RISING);
+
   
-  if (weapon_type == 0){
+
+
+}
+
+
+bool detect_hit(int pin) {
+  if (digitalRead(pin) == HIGH) {
+    return true;
+  }
+  return false;
+}
+
+int lameRightRead;
+int lameLeftRead;
+elapsedMillis sleeptime = 0;
+
+
+bool lockoutR = false;
+bool lockoutL = false;
+// if the button is held down
+
+void foil(void) {
+  while (1) {
+    //whSerial.println((micros() - mic));
+    //mic = micros();
+    if (weapon_change){
+      weapon_change=false;
+      return;
+    }
+
+    
+    if (!lockoutL){
+      lameLeftRead = analogRead(LEFTPLAYER_LAME);
+      Serial.print("READING");
+    }
+    if (!lockoutR){
+      
+      lameRightRead = analogRead(RIGHTPLAYER_LAME);
+    }
+    if (detect_hit(LEFTPLAYER_WEAPON) && 0) {
+      lockoutL = false;
+      lockoutR = true;
+      Serial.print("UNPLUGEDLET");
+      // if smaller than 900, means that lame is being touched......
+      if (lameRightRead < 900) {
+        digitalWrite(LEFTPLAYER_OUT, HIGH);
+      }
+      // else it is off target
+      else {
+        digitalWrite(LEFTPLAYER_OFF, HIGH);
+        Serial.println(lameRightRead);
+      }
+
+      // wait for a hit
+      timer0 = 0;
+      while (timer0 <= DOUBLE_TIME) {
+        if (detect_hit(RIGHTPLAYER_WEAPON)) {
+          lockoutL = true;
+          // if the hit is on
+          if (analogRead(LEFTPLAYER_LAME) < 900)
+          {
+            digitalWrite(RIGHTPLAYER_OUT, HIGH);
+            break;
+          }
+          else
+          {
+            digitalWrite(RIGHTPLAYER_OFF, HIGH);
+            break;
+          }
+        }
+      }
+      turn_off();
+      continue;
+    }
+    
+  if (detect_hit(RIGHTPLAYER_WEAPON)) {
+      Serial.print("UNPLUGEDRIGET");
+      Serial.println(lameLeftRead);
+      lockoutL = true;
+      lockoutR = false;
+      
+      // if smaller than 900, means that lame is being touched......
+      if (lameLeftRead < 900) {
+        Serial.println("HERER");
+        digitalWrite(RIGHTPLAYER_OUT, HIGH);
+      }
+      // else it is off target
+      else {
+        digitalWrite(RIGHTPLAYER_OFF, HIGH);
+        Serial.println(lameLeftRead);
+      }
+
+      // wait for a hit
+      timer0 = 0;
+      while (timer0 <= DOUBLE_TIME) {
+        if (detect_hit(LEFTPLAYER_WEAPON)) {
+          // if the hit is on
+          lockoutR = true;
+          if (analogRead(RIGHTPLAYER_LAME) < 900)
+          {
+            digitalWrite(LEFTPLAYER_OUT, HIGH);
+            break;
+          }
+          else
+          {
+            digitalWrite(LEFTPLAYER_OFF, HIGH);
+            break;
+          }
+        }
+      }
+      turn_off();
+      continue;
+    }
+  lockoutL = false;
+  lockoutR = false;
+  }
+  
+}
+void sabre(void) {
+  ;
+}
+void epee(void) {
+  ;
+}
+
+
+void loop(void) {
+  Serial.println("ENTERINGLOOP");
+  if (weapon_type==0){
     foil();
   }
   else if (weapon_type == 1){
     sabre();
   }
-  
-}
-
-
-void setup() {
-  pinMode(LEFTPLAYER_OUT, OUTPUT);
-  pinMode(LEFTPLAYER_IN, INPUT);
-  pinMode(RIGHTPLAYER_OUT, OUTPUT);
-  pinMode(RIGHTPLAYER_IN, INPUT);
-  
-  
-  Serial.begin(9600);
-  Serial.print("Hello world.");
-  
-  attachInterrupt(digitalPinToInterrupt(SWORDINTERRUPT),sword_choose,RISING);
-  foil();// TEMP! TODO REMOVE
-  
-}
-
-
-void foil(void){
-  if (detect_player(LEFTPLAYER_IN)){
-    Serial.print("UNPLUGEDLET");
-    digitalWrite(LEFTPLAYER_OUT, HIGH);
-    if (wait_hit(LEFT) == true){
-      digitalWrite(RIGHTPLAYER_OUT, HIGH);}
- 
-    
-    turn_off();
-    
+  else{
+    epee();
   }
-  else if(detect_player(RIGHTPLAYER_IN)){
-    Serial.print("UNPLUGEDRIT");
-    digitalWrite(RIGHTPLAYER_OUT, HIGH);
-    
-    if (wait_hit(RIGHT) == true){
-      digitalWrite(LEFTPLAYER_OUT, HIGH);}
-    turn_off();
-  }
-  
 }
-void sabre(void){
-  ;
-}
-
-
-void loop(){;}
